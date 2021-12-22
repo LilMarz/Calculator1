fdfb07f612ae86784a5a3c3b59cb566560655a37
akash <ian.bhuiyan@willamettevalleyacademy.org>
2021-12-22 13:23:28 -0800

    Here is the while loop and the division code that I was supposed to supply.

--- calculator.py
+++ calculator.py
@@ -1,3 +1,31 @@
 #It will calculate few calculations lol
 #Imagine being Aarosh, or Suhrid or Ekush (IAN)))))))))))))))))))))))))))))))))
-#i dojtn34tno q43ij qronq4j9rfhjnegwjwbvf from ian
+#Here is the code that I need to put (btw im doing the division part and the while loop)
+def division():
+    bruh = int(input("INSERT ANSWER HERE FOR THE NUMERO UNO "))
+    bruh_2 = int(input("INSERT ANSWER HERE FOR THE NUMERO DOS "))
+    if bruh_2 == 0 or bruh_2 < 0:
+        bruh_2 = int(input("INSERT ANSWER HERE FOR THE NUMERO DOS AGAIN"))
+    bruh_ans = bruh / bruh_2
+    print("Your bruh moment be like... ", bruh_ans)
+
+def float_division():
+    bruh = float("INSERT ANSWER HERE FOR THE NUMERO UNO ")
+    bruh_2 = float("INSERT ANSWER HERE FOR THE NUMERO DOS ")
+    if bruh_2 == 0 or bruh_2 < 0:
+        bruh_2 = int(input("INSERT ANSWER HERE FOR THE NUMERO DOS AGAIN"))
+    bruh_ans = bruh / bruh_2
+    print("Your bruh moment be like... ", bruh_ans)
+
+while True:
+    bruh_cal = input("Bruh u wanna calculate stuff or nah and btw if u need to divide decimals write float_div")
+    if bruh_cal == "nah":
+        break
+    elif bruh_cal == "yes":
+        bruh_operator = input("Which bruh moment operator would u like to use")
+        if bruh_operator == "div":
+            division()
+        elif bruh_operator == "float_div":
+            float_division()
+        else:
+            break
