fa3eb3a781da171461ca101a3d770b308bfda486
akash <ian.bhuiyan@willamettevalleyacademy.org>
2021-12-22 12:36:51 -0800

    THis i sformn INAb

--- calculator.py
+++ calculator.py
@@ -1,2 +1,3 @@
 #It will calculate few calculations lol
-#Imagine being Aarosh, or Suhrid or Ekush (IAN))))))))))))))))))))))))))))))
+#Imagine being Aarosh, or Suhrid or Ekush (IAN)))))))))))))))))))))))))))))))))
+#i dojtn34tno q43ij qronq4j9rfhjnegwjwbvf from ian
