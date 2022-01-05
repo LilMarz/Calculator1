99ce4db80691b7061de8a01f17fc26b53db80816
Lil Marz <94194188+LilMarz@users.noreply.github.com>
2022-01-04 20:27:44 -0800

    This is the addition code coming in from Aarosh

--- calculator.py
+++ calculator.py
@@ -1,1 +1,17 @@
 #I like to steal your cookies then dip them in the soda dispenser
+#Imagine being a bunyan plus going to a school with 21 kids plus having no arm nor leg hair and not being in summa
+#Imagine not having leg hair and having only indian friends and being hot
+#Imagine not being in summa and using discord during school and being a purid bacha
+
+def addition():
+    num = int(input("Give me number pls"))
+    num2 = int(input("Give me another number bruh bing chilling sussy boy"))
+    numans = num + num2
+    return numans
+
+
+print ("ur hot mama cita answer is", addition())
+
+
+
+
