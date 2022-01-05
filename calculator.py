dc5ed7a8b0307cac49887f0211cd68b9d529dafb
Oozegy <94505484+Oozegy@users.noreply.github.com>
2022-01-04 20:33:35 -0800

    fart

--- calculator.py
+++ calculator.py
@@ -1,2 +1,10 @@
 #It will calculate few calculations lol
 #this is from 21 life goes oin and oin and oin
+def subtraction():
+    sub1 = int(input("What is your first numberrrr"))
+    sub2 = int(input("what is your second numberrrrrrrr"))
+    if sub2 < 0:
+        sub2 = int(input("Put you second numberrr again hee hee hee haw"))
+    sub_answerr = sub1 - sub2
+    print("your answer is", sub_answerr)
+
