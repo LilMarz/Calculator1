c8cfd261b06015a06a2aa4122afad6ec350e3345
NewZew <481822@bsd48.org>
2022-01-04 20:15:35 -0800

    this is suhrid

--- calculator.py
+++ calculator.py
@@ -1,2 +1,15 @@
 #It will calculate few calculations lol
 #hello yeet
+#Multiplying code
+
+def multiply():
+    print("What is your first number?")
+    num1 = int(input())
+    print("What is your second number?")
+    num2 = int(input())
+    answer = num1 * num2
+    return answer
+
+print("Your final answer is", multiply())
+
+
